192fa3f03f4bcefac419b3eb7a4d5642e7d83eb0
Djany Mouton <djany.developpement@gmail.com>
2022-12-15 18:29:03 +0100

    Add TypeScript code

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,29 @@
 "use strict";
-let bonjour = "bonjour";
+let whiteShare = document.querySelector('#share__image');
+let blackShare = document.querySelector('#share__link');
+let user = document.querySelector('.user');
+let network = document.querySelector('.network');
+function showSocialNetwork() {
+    whiteShare === null || whiteShare === void 0 ? void 0 : whiteShare.addEventListener('click', () => {
+        if (user.style.display == "flex") {
+            user.style.display = "none";
+            network.style.display = "flex";
+        }
+        else {
+            user.style.display = "flex";
+            network.style.display = "none";
+        }
+    });
+    blackShare === null || blackShare === void 0 ? void 0 : blackShare.addEventListener('click', () => {
+        if (network.style.display == "flex") {
+            user.style.display = "flex";
+            network.style.display = "none";
+        }
+        else {
+            user.style.display = "none";
+            network.style.display = "flex";
+        }
+    });
+}
+;
+showSocialNetwork();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-let bonjour: string = "bonjour";
+let whiteShare  = document.querySelector<HTMLButtonElement>('#share__image')!;
+let blackShare  = document.querySelector<HTMLButtonElement>('#share__link')!;
+let user        = document.querySelector<HTMLBodyElement>('.user')!;
+let network     = document.querySelector<HTMLBodyElement>('.network')!;
+
+
+
+function showSocialNetwork() {
+    whiteShare?.addEventListener('click', () => {
+        if (user.style.display == "flex") {
+            user.style.display = "none";
+            network.style.display = "flex";
+        } else {
+            user.style.display = "flex";
+            network.style.display = "none";
+        }
+    });
+    blackShare?.addEventListener('click', () => {
+        if (network.style.display == "flex") {
+            user.style.display = "flex";
+            network.style.display = "none";
+        } else {
+            user.style.display = "none";
+            network.style.display = "flex";
+        }
+    });
+};
+showSocialNetwork();
+
+
+
